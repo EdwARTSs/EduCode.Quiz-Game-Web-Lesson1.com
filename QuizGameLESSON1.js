2f5106dfd226c57d057c0ccb0f388572523383ce
EdwART <122360856+EdwARTSs@users.noreply.github.com>
2023-01-12 22:09:25 +0800

    Add files via upload

---
+++ QuizGameLESSON1.js
@@ -0,0 +1,94 @@
+/*QUESTIONS FOR THE QUIZ LESSON 1*/
+
+const questions = [
+	{
+		question: 'The first version of HTML was written by?',
+		answers: [
+			{text: 'Tim Berners-Lee', correct: true},
+			{text: 'Timmy Berners-Lee', correct: false},
+			{text: 'Tim Bern-Lee', correct: false},
+			{text: 'Tim Berningham-Lee', correct: false}
+		]
+	},
+	{
+		question: 'Stands for Hyper Text Markup Language',
+		answers: [
+			{text: 'HML', correct: false},
+			{text: 'HTML', correct: true},
+			{text: 'HTL', correct: false},
+			{text: 'H_TML', correct: false}
+		]
+	},
+	{
+		question: 'This is the latest version of HTML',
+		answers: [
+			{text: 'HTML 1', correct: false},
+			{text: 'HTML 3', correct: false},
+			{text: 'HTML 5', correct: true},
+			{text: 'HTML 5.6', correct: false}
+		]
+	},
+	{
+		question: 'A kind of html tag that defines a large heading',
+		answers: [
+			{text: 'h1 or also known as html tag', correct: false},
+			{text: 'h1 or also known as heads tag', correct: false},
+			{text: 'h1 or also known as tag for head', correct: false},
+			{text: 'h1 or also known as header tag', correct: true}
+		]
+	},
+	{
+		question: 'A kind of html tag that defines a paragraph',
+		answers: [
+			{text: 'p or also known as paragraph tag', correct: true},
+			{text: 'p also known as paragraphing tag', correct: false},
+			{text: 'p also known as tags for paragraph', correct: false},
+			{text: 'p also known as paraphrase tag', correct: false}
+		]
+	},
+	{
+		question: 'Declaration defines that this document is an HTML5 document',
+		answers: [
+			{text: '<DOCTYPE html>', correct: false},
+			{text: '<!DOCTYPE html>', correct: true},
+			{text: '<DOCTYPE HTML5>', correct: false},
+			{text: '<DOCTYPE html5>', correct: false}
+		]
+	},
+	{
+		question: 'This tag specifies the title of the webpage',
+		answers: [
+			{text: '<caption>', correct: false},
+			{text: '<titling>', correct: false},
+			{text: '<title>', correct: true},
+			{text: '<titles>', correct: false}
+		]
+	},
+	{
+		question: 'This tag defines the body of the HTML document',
+		answers: [
+			{text: '<body_html>', correct: false},
+			{text: '<html.body>', correct: false},
+			{text: '<body of html>', correct: false},
+			{text: '<body>', correct: true}
+		]
+	},
+	{
+		question: 'This contains the meta information about the HTML page',
+		answers: [
+			{text: '<head>', correct: true},
+			{text: '<header>', correct: false},
+			{text: '<html>', correct: false},
+			{text: '<footer>', correct: false}
+		]
+	},
+	{
+		question: 'This is a framework of the webpage, similar to the skeletal system or framework of a human body',
+		answers: [
+			{text: 'CSS', correct: false},
+			{text: 'HTML', correct: true},
+			{text: 'JS', correct: false},
+			{text: 'PHP', correct: false}
+		]
+	}
+]
